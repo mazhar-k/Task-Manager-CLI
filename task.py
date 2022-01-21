81d0b0cbf9f474570dd1898d984b4f040f9f456c
mazhar-k <64404507+mazhar-k@users.noreply.github.com>
2022-01-21 22:16:10 +0530

    Update task.py

--- task.py
+++ task.py
@@ -15,7 +15,7 @@ completed_path = os.path.join(base_dir, "completed.txt")
 # help function
 def help():
     usage = '''Usage :-
-$ .\task add 2 hello world     # Add a new item with priority 2 and text "hello world" to the list
+$ .\task add 2"hello world"     # Add a new item with priority 2 and text "hello world" to the list
 $ .\task ls                    # Show incomplete priority list items sorted by priority in ascending order
 $ .\task del NUMBER   # Delete the incomplete item with the given priority number
 $ .\task done NUMBER  # Mark the incomplete item with the given PRIORITY_NUMBER as complete
